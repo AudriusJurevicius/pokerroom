fdc46280f1c57b2ca79dba1e7388ea6a5cc989a6
Audrius Jurevicius <Audrius.Jurevicius@pm.me>
2021-03-24 21:45:36 +0000

    Sutvarkyta navigacija

--- navigation.js
+++ navigation.js
@@ -13,25 +13,23 @@ document.write(`\
                       Tickets\
                     </a>\
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
-                      <a class="dropdown-item" href="tickets.html">Ticket Encashment</a>\
-                      <a class="dropdown-item" href="PPL-Dollars.html">PPL Dollar Encashment</a>\
-                      <a class="dropdown-item" href="issue-ticket.html">Add tickets</a>\
-                      <a class="dropdown-item" href="issue-packages.html">PPL Packages</a>\
+                      <a class="dropdown-item" href="issue-ticket.html">Issue Tickets</a>\
+					  <a class="dropdown-item" href="tickets.html">View Tickets</a>\
                     </div>\
                   </li>\
-                  <li class="nav-item active">\
-                    <a class="nav-link" href="payouts.html">Payouts</a>\
+                  <li class="nav-item disabled">\
+                    <a class="nav-link" href="#">(unused)</a>\
                   </li>\
-                  <li class="nav-item active">\
-                    <a class="nav-link" href="newODsetup.html">Percentage Payouts</a>\
+                  <li class="nav-item disabled">\
+                    <a class="nav-link" href="#">(unused)</a>\
                   </li>\
                   <li class="nav-item dropdown">\
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
-                      Descriptions\
+                      Descriptions (in progress)\
                     </a>\
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
-                      <a class="dropdown-item disabled" href="#">Create partypoker Lobby Description</a>\
-                      <a class="dropdown-item disabled" href="#">Preview partypoker Lobby Description</a>\
+                      <a class="dropdown-item disabled" href="#">Create</a>\
+                      <a class="dropdown-item disabled" href="#">Preview</a>\
                       <div class="dropdown-divider"></div>\
                       <a class="dropdown-item disabled" href="#">Not yet done</a>\
                     </div>\
